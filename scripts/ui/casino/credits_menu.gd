cc0d01e5028449fd1cc1e23d1bc1efb2783bfc00
thejowe <joeldetorres123@gmail.com>
2026-08-27 19:43:59 -1200

    feat: pantalla de créditos accesible desde LobbyMenu
    - credits_menu.tscn/.gd: título, desarrollador, agradecimientos MIT
      (Godot Engine, GodotSteam, GUT), botón Volver
    - lobby_menu: botón Créditos que navega a la nueva escena
    - test_credits_menu.gd: instancia sin error, BackButton presente

---
+++ scripts/ui/casino/credits_menu.gd
@@ -0,0 +1,10 @@
+class_name CreditsMenu
+extends Control
+
+@onready var back_button: CasinoButton = $BackButton
+
+func _ready() -> void:
+	back_button.pressed.connect(_on_back_pressed)
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/lobby_menu.tscn")
